5b9a300f97227bbb2e8366131fb221ca69e713fe
Yazan Alqadery <yazanalqadery@gmail.com>
2025-08-11 12:08:53 +0400

    Added roles to access specific endpoints

--- src/main/java/com/yzn/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/yzn/securitydemo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.yzn.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration // --> Tells Spring that this class provides configuration to the application context
 @EnableWebSecurity // --> Tells Spring Boot to enable web security features in the application
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean // --> Mark this as bean
--- src/main/java/com/yzn/securitydemo/controller/GreetingsController.java
+++ src/main/java/com/yzn/securitydemo/controller/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.yzn.securitydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,18 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    // Check Authorization before executing a method, and you can specify conditions that need to be true for the method to execute
+    @PreAuthorize("hasRole('USER')") // -> Telling Spring Security, that only users with a specific role to access this
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello, Admin!";
     }
 }
